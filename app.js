54b45315a7ea49e79d383a7da3cebdae81eb75a4
febingeevarghese <febingeevarghese@gmail.com>
2019-09-28 15:51:31 +0600

    for  hosting

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const path=require('path');
 const app=express();
 app.use(express.stastic(__dirname + '/dist/my-app'));
 app.get('/*', function(req, res){
-    res.sendFile(path.join(-__dirname + '/dist/my-app/index.html'))
-);
+    res.sendFile(path.join(__dirname + '/dist/my-app/index.html'))
+});
 app.listen(process.env.port || 3000);
-}
